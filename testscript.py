5f575de5975ca76f68581d7bc83670395320ede6
Cameron Longfellow <clongfellow@hawk.iit.edu>
2022-02-14 20:38:47 -0600

    fields update

--- testscript.py
+++ testscript.py
@@ -2,14 +2,17 @@ import psutil
 import mysql.connector as connector
 import csv
 
+#TODO 
+#listen on ftp 
 #Assumes the database is already set up
+
 #edit to match server
 db = connector.connect(host="localhost",user="root",
                   passwd="server",db="ipro_db")
 #change to match the new sql server parameters
 add_proc = ("INSERT INTO processes "
-               "(PROCESS_ID, PROCESS_NAME, MEM_USAGE, CPU_USAGE)"
-               "VALUES (%i, %s, %i, %i)")
+               "(TIME, MACHINE_ID, PROCESS_NAME, MEMORY_UTILIZATION, CPU_UTILIZATION, MEMORY_USED, THREADS, USER, PATH, ID)"
+               "VALUES (%s, %s, %s, %d, %d, %d, %d, %s, %s, %d )")
 c = db.cursor()
 #cursor.execute(sql, list(test.values()))
 with open('foo.csv', newline='') as csvfile:
@@ -20,6 +23,6 @@ with open('foo.csv', newline='') as csvfile:
         if row[0] == "time":
             continue
         # row is a list, each position is a string based on the csv. 
-        c.execute(add_proc, (1, row[2], row[3], row[4]))
+        c.execute(add_proc, (row[0], row[1], row[2], int(row[3]), int(row[4]), int(row[5]), row[6], row[7], int(row[8]))
 query = "SELECT * from processes"
 c.execute(query)
